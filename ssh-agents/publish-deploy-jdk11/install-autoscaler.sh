aab69ab90714519ea8b93a7a3acc622ca20d98fa
jross <jordan.ross@merrillcorp.com>
2020-04-29 09:59:17 -0500

    Renaming app autsocaler plugin binary

--- ssh-agents/publish-deploy-jdk11/install-autoscaler.sh
+++ ssh-agents/publish-deploy-jdk11/install-autoscaler.sh
@@ -2,6 +2,6 @@
 
 # Need to run as jenkins user! su - jenkins
 
-cf install-plugin app-autoscaler-plugin -f
+cf install-plugin app-autoscaler-plugin-2.0.91 -f
 cd ../
-cf install-plugin app-autoscaler-plugin -f
+cf install-plugin app-autoscaler-plugin-2.0.91 -f
