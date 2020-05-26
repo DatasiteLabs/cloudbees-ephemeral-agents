dd536e1b7b113e2fd59c371e1cfa6989ee8d8511
Jordan Ross <jordan.ross@datasite.com>
2020-05-26 12:41:16 -0500

    Fix autoscaling plugins (#29)
    * Getting cf autoscaler plugins installed
    
    * Removing copy plugin command
    
    * Fixing scripts so cf plugins are installed as the jenkins user
    
    * updating publish-deploy-jdk8 image
    
    Co-authored-by: jross <jordan.ross@merrillcorp.com>

--- ssh-agents/publish-deploy-jdk8/build.sh
+++ ssh-agents/publish-deploy-jdk8/build.sh
@@ -4,14 +4,14 @@ usage() {
     cat <<END
     ./build.sh [-h] -u some-token-value -t your-build-tag -p path-to-your-dockerfile
 
-    Used to build a docker image containing the Pivnet Cloud Foundry Autoscaler plugin. 
+    Used to build a docker image containing the Pivnet Cloud Foundry Autoscaler plugin.
     In order to download it, you will need a uaa token from the Pivotal Newtork. This can
     be obtaioned through the Pivotal Network UI, and will be tied to your Pivotal Network
     account.
 
     -u = uaa_token, the Pivotal Network token the script will exchange for your access token,
          which can then be passed safely to the Dockerfile's args because it will expire.
-    
+
     -t = the docker build tag you want passed to the script
 
     -p = the path to your Dockerfile. If left blank, the script will assume you are executing
@@ -20,7 +20,7 @@ END
 }
 
 while getopts ":h:u:t:p:" opt; do
-    case $opt in 
+    case $opt in
         h) usage
            exit 0
            ;;
@@ -41,5 +41,5 @@ if [ -z ${docker_path+x} ]; then docker_path=$(pwd); fi;
 
 access_token=$(curl -X POST https://network.pivotal.io/api/v2/authentication/access_tokens -d "{\"refresh_token\":\"$uaa_token\"}" -s 2>&1 | sed 's/{"access_token":"\(.*\)"}/\1/')
 docker build -t $docker_tag $docker_path --build-arg token=$access_token
-sleep 5m # this kills the API credential that would appear in the container history by waiting for it to expire
-docker push $docker_tag # if you're pushing to a private repository, make sure you've taken care of whatever credentials you need!
+# sleep 5m # this kills the API credential that would appear in the container history by waiting for it to expire
+# docker push $docker_tag # if you're pushing to a private repository, make sure you've taken care of whatever credentials you need!
